027575f25548b2333b0c3f95b6d0188f853033e4
robert <robertbassettIII@gmail.com>
2024-03-27 11:47:54 -0700

    added serial and parallel version

--- simplex_projection/src/python/simplex_projection.py
+++ simplex_projection/src/python/simplex_projection.py
@@ -6,18 +6,30 @@ this_path = os.path.dirname(__file__)
 
 #import the library.
 
-lib = cdll.LoadLibrary(os.path.join(this_path, '..', '..', 'build', 'simplex_proj.so'))
+plib = cdll.LoadLibrary(os.path.join(this_path, '..', '..', 'build', 'parallel_simplex_proj.so'))
+slib = cdll.LoadLibrary(os.path.join(this_path, '..', '..', 'build', 'serial_simplex_proj.so'))
 
 #argument types of swap_min_max
-lib.simplex_proj.argtypes = [ndpointer(c_double, ndim=2, flags="C_CONTIGUOUS"),\
+plib.simplex_proj.argtypes = [ndpointer(c_double, ndim=2, flags="C_CONTIGUOUS"),\
                             ndpointer(c_double, ndim=2, flags="C_CONTIGUOUS"),
                             ndpointer(c_double, ndim=2, flags="C_CONTIGUOUS"),
                             c_long,
                             c_long]
-#return type
-lib.simplex_proj.restype = c_void_p
+slib.simplex_proj.argtypes = [ndpointer(c_double, ndim=2, flags="C_CONTIGUOUS"),\
+                            ndpointer(c_double, ndim=2, flags="C_CONTIGUOUS"),
+                            ndpointer(c_double, ndim=2, flags="C_CONTIGUOUS"),
+                            c_long,
+                            c_long]
 
-def simplex_proj(arr, u, cumsum):
+#return type
+plib.simplex_proj.restype = c_void_p
+slib.simplex_proj.restype = c_void_p
+
+def simplex_proj_parallel(arr, u, cumsum):
     '''arr, u, and cumsum are all double precision arrays of dimension (m,n)'''
-    lib.simplex_proj(arr, u, cumsum, arr.shape[0], arr.shape[1])
+    plib.simplex_proj(arr, u, cumsum, arr.shape[0], arr.shape[1])
+
+def simplex_proj_serial(arr, u, cumsum):
+    '''arr, u, and cumsum are all double precision arrays of dimension (m,n)'''
+    slib.simplex_proj(arr, u, cumsum, arr.shape[0], arr.shape[1])
 
--- simplex_projection/src/python/tests.py
+++ simplex_projection/src/python/tests.py
@@ -6,14 +6,19 @@ import numpy as np
 import time
 
 m = 1000
-n = 1000
+n = 10000
 print(f"Working with {m} x {n} arrays")
 np.random.seed(0)
 arr = np.random.randn(m, n)
 u = np.empty((m, n), dtype=np.float64)
 cumsum = np.empty((m, n), dtype=np.float64)
 
-print("Running simplex_proj")
+print("Running simplex_proj in serial")
 start = time.time()
-simplex_projection.simplex_proj(arr, u, cumsum)
+simplex_projection.simplex_proj_serial(arr, u, cumsum)
+print(f'Completed in {time.time() - start} seconds')
+
+print("Running simplex_proj in parallel")
+start = time.time()
+simplex_projection.simplex_proj_parallel(arr, u, cumsum)
 print(f'Completed in {time.time() - start} seconds')
